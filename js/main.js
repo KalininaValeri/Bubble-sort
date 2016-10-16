c7f07c186e1616b1a76e157852f09895c963d993
KalininaValeri <kalinina.valeri@gmail.com>
2016-10-16 20:16:29 +0700

    sorted piz*decgit add --allgit add --all!

--- js/main.js
+++ js/main.js
@@ -22,7 +22,6 @@ function getInputValue() {
     for (var i = 0; i < array.length; i++) {
         array[i] = Number(array[i]);
     }
-    console.log(array);
 
     return array;
 }
@@ -34,8 +33,79 @@ function createTable() {
     for (var i = 0; i < array.length; i++) {
         $tr.append('<td>' + array[i] + '</td>')
     }
+}
+
+function timeoutWrapper(callBack, speed) {
+    var interval = setInterval(function () {
+        callBack();
+        clearInterval(interval);
+    }, speed || 500);
+}
+
+function checkElements(item1, item2) {
+   
+}
+
+function sortTable() {
+    //todo сжечь огнем
+    $('#input-src').val('88, 13, 87, 18, 54, 48, 7, 47, 65, 59');
+    createTable();
+
+    var $arrayTd = $('#table-sort').find('td');
+    var lenghtArray = $arrayTd.length;
+
+    function startFirstStep() {
+        var i = 0;
+
+        --lenghtArray;
+
+        function sort() {
+            $arrayTd.removeClass('danger warning');
+
+            if (i < lenghtArray) {
+                var $item1 = $($arrayTd[i]);
+                var $item2 = $($arrayTd[i + 1]);
+                var value1 = Number($item1.text());
+                var value2 = Number($item2.text());
+
+                $item1.addClass('warning');
+                $item2.addClass('warning');
 
 
+                if (value1 > value2) {
+                    timeoutWrapper(function () {
+                        $item1.removeClass('warning');
+                        $item1.addClass('danger');
+                        $item2.removeClass('warning');
+                        $item2.addClass('danger');
+
+                        $item1.text(value2);
+                        $item2.text(value1);
+
+                        newIteration();
+                    }, 400);
+
+                    return;
+                }
+            }
+
+            newIteration();
+        }
+        
+        function newIteration() {
+            if (i < lenghtArray) {
+                i++;
+                timeoutWrapper(sort, 400);
+            } else {
+                $($arrayTd[lenghtArray]).addClass('success');
+                if (lenghtArray > 0) timeoutWrapper(startFirstStep, 400);
+            }
+        }
+
+        sort();
+    }
+
+    startFirstStep();
 }
 
 $(function () {
@@ -43,4 +113,6 @@ $(function () {
         var array = getRandomArray();
         $('#input-src').val(array.join(', '));
     });
+
+
 });
